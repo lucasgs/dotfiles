c977cd0c1fa6c444493243f0ce76ad9b04781543
Lucas Sanchez <lucasgsanchez@gmail.com>
2022-07-16 17:40:45 -0300

    Move autocmd for packer plugins sync

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -1,3 +1,12 @@
+-- Format on save buffer
 --vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 -- or
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
