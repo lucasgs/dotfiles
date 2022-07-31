e4ca208f8decae52727c6a374a95368f184c288c
Lucas Sanchez <lucasgsanchez@gmail.com>
2022-07-31 10:53:31 -0300

    Add mappings for terminal and win resizing

--- .config/nvim/lua/user/plugins/bufferline.lua
+++ .config/nvim/lua/user/plugins/bufferline.lua
@@ -16,7 +16,7 @@ bufferline.setup {
     show_buffer_icons = false, -- disable filetype icons for buffers
     show_buffer_close_icons = false,
     name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-      return buf.bufnr .. ": " .. buf.name
+      return buf.bufnr .. ":[" .. buf.name .. "]"
     end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
   }
