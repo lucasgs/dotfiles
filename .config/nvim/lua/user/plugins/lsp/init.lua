2ca8e6719e111554956c600ca3b4be603e5d8110
Lucas Sanchez <lucasgsanchez@gmail.com>
2022-10-09 09:21:23 -0300

    nvim - Add kotlin language support

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -90,7 +90,8 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'sumneko_lua', 'rust_analyzer' }
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'sumneko_lua', 'rust_analyzer',
+  'kotlin_language_server' }
 
 -- Call setup
 for _, server in ipairs(servers) do
