eaf2402dfa4c032c7c928ac72894ed3357880698
Lucas Sanchez <lucasgsanchez@gmail.com>
2023-10-13 12:56:03 -0300

    nvim - add harpoon keybindings

--- nvim/.config/nvim/lua/user/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/user/plugins/harpoon.lua
@@ -24,9 +24,9 @@ global_settings = {
 }
 
 local keymap = vim.keymap.set
-keymap('n', 'ha', require('harpoon.mark').add_file, { desc = 'Harpoon add' })
-keymap('n', 'hc', require('harpoon.mark').clear_all, { desc = 'Harpoon clear all' })
-keymap('n', 'hn', require('harpoon.ui').nav_next, { desc = 'Harpoon next' })
-keymap('n', 'hp', require('harpoon.ui').nav_prev, { desc = 'Harpoon prev' })
-keymap('n', 'hm', require('harpoon.ui').toggle_quick_menu, { desc = 'Harpoon quick menu' })
+keymap('n', '<leader>ha', require('harpoon.mark').add_file, { desc = 'Harpoon add' })
+keymap('n', '<leader>hc', require('harpoon.mark').clear_all, { desc = 'Harpoon clear all' })
+keymap('n', '<leader>hn', require('harpoon.ui').nav_next, { desc = 'Harpoon next' })
+keymap('n', '<leader>hp', require('harpoon.ui').nav_prev, { desc = 'Harpoon prev' })
+keymap('n', '<leader>hm', require('harpoon.ui').toggle_quick_menu, { desc = 'Harpoon quick menu' })
 -- vim.keymap.set('n', 'hm', ':Telescope harpoon marks<CR>')
