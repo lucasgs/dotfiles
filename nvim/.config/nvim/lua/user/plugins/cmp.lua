0a9bdf444775bcaea791af80943c8c47681f99ab
Lucas Sanchez <lucasgsanchez@gmail.com>
2023-10-18 09:14:26 -0300

    nvim - fix cmp error

--- nvim/.config/nvim/lua/user/plugins/cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/cmp.lua
@@ -8,15 +8,8 @@ if not luasnip_status_ok then
   return
 end
 
-luasnip.config.setup {}
-
--- local s = luasnip.snippet
--- luasnip.add_snippets ("all",  {
---   luasnip.parser.parse_snippet({trig = "expand"}, "This is what was expanded!"),
---   luasnip.parser.parse_snippet({trig = "lsp"}, "$1 is ${2|hard,easy,challenging|}"),
--- })
-
 require("luasnip.loaders.from_vscode").lazy_load()
+luasnip.config.setup {}
 
 local icons = require("user.icons")
 local kind_icons = icons.kind
@@ -53,94 +46,36 @@ cmp.setup {
   },
 
   -- Key mapping
-  mapping = {
-    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-c>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
-    ["<m-j>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    ["<m-k>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    ["<m-c>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    ["<S-CR>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<Right>"] = cmp.mapping.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif check_backspace() then
-        -- cmp.complete()
-        fallback()
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      elseif luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end, { 'i', 's' }),
   },
 
-  -- formatting = {
-  -- fields = { "abbr", "kind", "menu" },
-  -- fields = { "kind", "abbr", "menu" },
-  -- format = function(entry, vim_item)
-  -- Kind icons
-  -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-  -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-  -- vim_item.kind = string.format('%s (%s)', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-  -- vim_item.kind = kind_icons[vim_item.kind]
-  -- vim_item.menu = ({
-  --   -- luasnip = "[Snippet]",
-  --   -- buffer = "[Buffer]",
-  --   -- path = "[Path]",
-  --   nvim_lsp = "",
-  --   nvim_lua = "",
-  --   luasnip = "",
-  --   buffer = "",
-  --   path = "",
-  --   emoji = "",
-  -- })[entry.source.name[Path]]
-  -- return vim_item
-  -- end,
-  -- },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = true,
