0be1872ee0de78b72bff61eb68ac4dd31086a548
Lucas Sanchez <lucasgsanchez@gmail.com>
2024-06-13 00:06:42 -0300

    nvim - Oil, show hidden files

--- nvim/.config/nvim/lua/user/plugins/oil.lua
+++ nvim/.config/nvim/lua/user/plugins/oil.lua
@@ -3,4 +3,16 @@ if not status_ok then
     return
 end
 
-oil.setup()
+oil.setup({
+    view_options = {
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+            return vim.startswith(name, ".")
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+    }
+})
