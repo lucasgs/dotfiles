ff57461c3d69afa5f284ceaf3aff764ec21677a6
Lucas Sanchez <lucasgsanchez@gmail.com>
2024-09-04 17:40:38 -0300

    nvim - which-key, remove call to the register function

--- nvim/.config/nvim/lua/user/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/user/plugins/whichkey.lua
@@ -1,13 +1,6 @@
 local status_ok, whichkey = pcall(require, "which-key")
 if not status_ok then
-  return
+    return
 end
 
 whichkey.setup({})
-
-whichkey.register({
-  h = {
-    name = "harpoon",
-    a = { function() require('harpoon.mark').add_file() end, "Mark file" }
-  }
-}, { prefix = "<leader>" })
