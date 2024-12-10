8ceb5ec93b476e5bca45cdb1387dc54e88687469
Lucas Sanchez <lucasgsanchez@gmail.com>
2024-12-10 15:23:58 -0300

    Disable treesitter context

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -3,6 +3,15 @@ if not status_ok then
     return
 end
 
+local context_ok, context = pcall(require, "treesitter-context")
+if not context_ok then
+    return
+end
+
+context.setup {
+    enable = false,
+}
+
 configs.setup({
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bash", "html", "javascript" },
     sync_install = false,
@@ -19,23 +28,23 @@ configs.setup({
             node_decremental = "V",
         },
     },
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V',  -- linewise
-                ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            include_surrounding_whitespace = true,
-        },
-    },
+    -- textobjects = {
+    --     select = {
+    --         enable = true,
+    --         lookahead = true,
+    --         keymaps = {
+    --             ["af"] = "@function.outer",
+    --             ["if"] = "@function.inner",
+    --             ["ac"] = "@class.outer",
+    --             ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+    --             ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+    --         },
+    --         selection_modes = {
+    --             ['@parameter.outer'] = 'v', -- charwise
+    --             ['@function.outer'] = 'V',  -- linewise
+    --             ['@class.outer'] = '<c-v>', -- blockwise
+    --         },
+    --         include_surrounding_whitespace = true,
+    --     },
+    -- },
 })
