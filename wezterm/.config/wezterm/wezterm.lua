5c2f8671eb7f6f677e84fa741d7f930e4c2f3f42
Lucas Sanchez <lucasgsanchez@gmail.com>
2024-12-26 12:44:16 -0300

    wezterm - refactor some options

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,25 +1,20 @@
-local wezterm                       = require 'wezterm'
-local act                           = wezterm.action
+local wezterm = require 'wezterm'
+local act     = wezterm.action
 
-local config                        = {}
-
-config.font                         = wezterm.font('Hack Nerd Font Mono')
-config.font_size                    = 14.0
+local config  = {}
+if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- config.color_scheme                 = 'tokyonight-storm'
---config.color_scheme                 = 'kanagawabones'
 config.color_scheme                 = 'Kanagawa (Gogh)'
 
-config.hide_tab_bar_if_only_one_tab = true
+config.font                         = wezterm.font_with_fallback({
+    { family = 'Hack Nerd Font Mono', scale = 1.2 }
+})
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar            = false
 config.tab_bar_at_bottom            = true
-
--- config.window_decorations = "RESIZE"
--- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-
--- window_background_opacity = 0.95,
+-- config.window_decorations           = "RESIZE"
 config.window_background_opacity    = 1.00
 
 config.initial_cols                 = 150
@@ -78,6 +73,11 @@ config.keys = {
         action = act.SwitchToWorkspace {
             name = 'default',
         },
+    },
+    {
+        key = 'x',
+        mods = 'LEADER',
+        action = act.CloseCurrentTab { confirm = false }
     },
     {
         key = 'n',
