54f141e93d29454b15b20fb56f94b2f8ea2069a1
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-02-11 09:50:15 -0300

    nvim - remove unused code from oil configs

--- nvim/.config/nvim/lua/user/plugins/oil.lua
+++ nvim/.config/nvim/lua/user/plugins/oil.lua
@@ -6,13 +6,5 @@ end
 oil.setup({
     view_options = {
         show_hidden = true,
-        -- This function defines what is considered a "hidden" file
-        is_hidden_file = function(name, bufnr)
-            return vim.startswith(name, ".")
-        end,
-        -- This function defines what will never be shown, even when `show_hidden` is set
-        is_always_hidden = function(name, bufnr)
-            return false
-        end,
     }
 })
