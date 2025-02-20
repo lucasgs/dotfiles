d2e983555b0772ed587102bdbb4f0cd1b7c83cf8
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-02-20 20:43:27 -0300

    nvim - add Nordic theme

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -78,6 +78,15 @@ local plugins = {
         priority = 1000,
     },
 
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('nordic').load()
+        end
+    },
+
     { "rose-pine/neovim", name = "rose-pine" },
 
     {
