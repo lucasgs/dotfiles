5fce590e9d52c8c5894150f552b0ef850df4e64c
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-02-25 12:04:11 -0300

    nvim - Update Conform config

--- nvim/.config/nvim/lua/user/autocmds.lua
+++ nvim/.config/nvim/lua/user/autocmds.lua
@@ -1,23 +1,31 @@
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    desc = 'Format current buffer before writing',
-    group = vim.api.nvim_create_augroup('OwnFormatWriting', { clear = true }),
-    callback = function()
-        vim.lsp.buf.format()
-    end,
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--     desc = 'Format current buffer before writing',
+--     group = vim.api.nvim_create_augroup('OwnFormatWriting', { clear = true }),
+--     callback = function()
+--         vim.lsp.buf.format()
+--     end,
+-- })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "Format current buffer before writing",
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
-vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
-    desc = "Highlight yanked selection",
-    group = vim.api.nvim_create_augroup('OwnYankHighlight', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	desc = "Highlight yanked selection",
+	group = vim.api.nvim_create_augroup("OwnYankHighlight", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    desc = "Run linter after writing",
-    group = vim.api.nvim_create_augroup('OwnLintWrite', { clear = true }),
-    callback = function()
-        require("lint").try_lint()
-    end,
+	desc = "Run linter after writing",
+	group = vim.api.nvim_create_augroup("OwnLintWrite", { clear = true }),
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -1,12 +1,16 @@
 local status_ok, conform = pcall(require, "conform")
 if not status_ok then
-  return
+    return
 end
 
 conform.setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "isort", "black" },
-    javascript = { { "prettierd", "prettier" } },
-  },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+    },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
 })
