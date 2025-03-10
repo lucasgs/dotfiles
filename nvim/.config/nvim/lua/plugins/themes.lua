ec3c74cfb9597a8e5a442774577229f27978e250
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-03-10 16:53:47 -0300

    nvim - add themes

---
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"tjdevries/colorbuddy.nvim",
+		opts = {},
+	},
+	{
+		"svrana/neosolarized.nvim",
+		dependencies = { "tjdevries/colorbuddy.nvim" },
+	},
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("everforest").setup({
+				background = "hard",
+			})
+		end,
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+}
