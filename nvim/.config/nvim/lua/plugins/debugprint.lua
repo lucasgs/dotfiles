08493913f617767cd3df7f1dc9f19b33834b1696
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-03-10 17:02:02 -0300

    nvim - add debugprint

---
+++ nvim/.config/nvim/lua/plugins/debugprint.lua
@@ -0,0 +1,10 @@
+return {
+	"andrewferrier/debugprint.nvim",
+	dependencies = {
+		"echasnovski/mini.nvim", -- Needed for :ToggleCommentDebugPrints(NeoVim 0.9 only)
+		-- and line highlighting (optional)
+	},
+	lazy = false,
+	version = "*",
+	opts = {},
+}
