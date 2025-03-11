1d6bd76e7f6442d478720cde68cf27c83f83a931
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-03-11 20:29:47 -0300

    nvim - lint pass

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
-return { 
-    "nvim-treesitter/nvim-treesitter",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+return {
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },  
-            autopairs = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    node_incremental = "v",
-                    node_decremental = "V",
-                },
-            },
-        })
-    end
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			autopairs = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					node_incremental = "v",
+					node_decremental = "V",
+				},
+			},
+		})
+	end,
 }
