bcdc594c8e91dbe4956762f58556941b8139eebf
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-03-14 10:13:13 -0300

    nvim - add jsonls language protocol

--- nvim/.config/nvim/lua/user/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/init.lua
@@ -1,11 +1,11 @@
-local lsp_status_ok, lspconfig = pcall(require, 'lspconfig')
+local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lsp_status_ok then
-    return
+	return
 end
 
-local cmp_status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+local cmp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_status_ok then
-    return
+	return
 end
 
 -- Show line diagnostics automatically in hover window
@@ -19,89 +19,101 @@ capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    -- Highlighting references
-    if client.server_capabilities.document_highlight then
-        vim.api.nvim_exec([[
+	-- Highlighting references
+	if client.server_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
-    end
+    ]],
+			false
+		)
+	end
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    local opts = { noremap = true, silent = true }
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { desc = 'Goto declaration' })
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { desc = 'Goto definition' })
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { desc = 'Hover documentation' })
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { desc = 'Goto implementation' })
-    -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { desc = 'Goto type definition' })
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { desc = 'Rename' })
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { desc = 'Code action' })
-    buf_set_keymap('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', { desc = 'Goto references' })
-    buf_set_keymap('n', '<leader>fu', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', { desc = 'Format buffer' })
-    buf_set_keymap('n', '<leader>sh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { desc = 'Signature help' })
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { desc = "Goto declaration" })
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Goto definition" })
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "Hover documentation" })
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "Goto implementation" })
+	-- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	-- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	-- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	-- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+	buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { desc = "Goto type definition" })
+	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename" })
+	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "Code action" })
+	buf_set_keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "Goto references" })
+	buf_set_keymap("n", "<leader>fu", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", { desc = "Format buffer" })
+	buf_set_keymap("n", "<leader>sh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "Signature help" })
 
-    buf_set_keymap('n', '<leader>dj', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = 'Diagnostic next' })
-    buf_set_keymap('n', '<leader>dk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = 'Diagnostic prev' })
-    buf_set_keymap('n', '<leader>dl', '<cmd>lua vim.diagnostic.setloclist()<CR>', { desc = 'Diagnostic set local list' })
+	buf_set_keymap("n", "<leader>dj", "<cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "Diagnostic next" })
+	buf_set_keymap("n", "<leader>dk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "Diagnostic prev" })
+	buf_set_keymap(
+		"n",
+		"<leader>dl",
+		"<cmd>lua vim.diagnostic.setloclist()<CR>",
+		{ desc = "Diagnostic set local list" }
+	)
 end
 
 -- Define `root_dir` when needed
 -- See: https://github.com/neovim/nvim-lspconfig/issues/320
 -- This is a workaround, maybe not work with some servers.
 local root_dir = function()
-    return vim.fn.getcwd()
+	return vim.fn.getcwd()
 end
 --
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
 local servers = {
-    'bashls',
-    -- 'pyright',
-    'pylsp',
-    -- 'ruff_lsp',
-    'clangd',
-    'html',
-    'cssls',
-    'ts_ls',
-    'lua_ls',
-    'rust_analyzer',
-    'kotlin_language_server',
-    'groovyls',
-    'gopls',
-    'sourcekit'
+	"bashls",
+	-- 'pyright',
+	"pylsp",
+	-- 'ruff_lsp',
+	"clangd",
+	"html",
+	"cssls",
+	"ts_ls",
+	"lua_ls",
+	"rust_analyzer",
+	"kotlin_language_server",
+	"groovyls",
+	"gopls",
+	"jsonls",
+	"sourcekit",
 }
-
 
 -- Call setup
 for _, server in ipairs(servers) do
-    local opts = {
-        on_attach = on_attach,
-        root_dir = root_dir,
-        capabilities = capabilities,
-        inlay_hints = { enabled = true },
-    }
+	local opts = {
+		on_attach = on_attach,
+		root_dir = root_dir,
+		capabilities = capabilities,
+		inlay_hints = { enabled = true },
+	}
 
-    local has_custom_opts, server_opts = pcall(require, "user.plugins.lsp.settings." .. server)
-    if has_custom_opts then
-        opts = vim.tbl_deep_extend("force", opts, server_opts)
-    end
+	local has_custom_opts, server_opts = pcall(require, "user.plugins.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
