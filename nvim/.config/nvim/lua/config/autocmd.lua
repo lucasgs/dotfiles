7455febbfed02e5f063b02b824908e55dc7afaab
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-04-16 14:15:23 -0300

    nvim - Add Google search autocmd

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -101,3 +101,10 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 		vim.opt.relativenumber = false
 	end,
 })
+
+vim.api.nvim_create_user_command("Google", function(o)
+	-- local escaped = require('socket.url').escape(o.args)
+	local escaped = vim.uri_encode(o.args)
+	local url = ("https://www.google.com/search?q=%s"):format(escaped)
+	vim.ui.open(url)
+end, { nargs = 1, desc = "just google it" })
