80f52a65f73869cd5a1597ab42fa345754f6d527
Lucas Sanchez <lucasgsanchez@gmail.com>
2025-06-04 09:53:50 -0300

    nvim - update avante providers

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -4,22 +4,28 @@ return {
 	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 	opts = {
 		-- provider = "ollama",
+		-- auto_suggestions_provider = "ollama",
+		-- cursor_applying_provider = "ollama",
 		provider = "gemini",
-		ollama = {
-			endpoint = "http://127.0.0.1:11434",
-			-- model = "deepseek-r1:latest",
-			model = "qwen2.5-coder:7b-instruct",
-		},
-		cursor_applying_provider = "ollama",
+		-- cursor_applying_provider = "gemini",
 		behaviour = {
 			enable_cursor_planning_mode = true, -- enable cursor planning mode!
 		},
-		vendors = {
-			deepseek = {
+		providers = {
+			ollama = {
+				api_key_name = "",
+				-- model = "deepseek-r1:latest",
+				model = "qwen2.5-coder:7b-instruct",
 				__inherited_from = "openai",
-				api_key_name = "DEEPSEEK_API_KEY",
-				endpoint = "https://api.deepseek.com",
-				model = "deepseek-chat",
+				endpoint = "http://127.0.0.1:11434",
+			},
+			vendors = {
+				deepseek = {
+					__inherited_from = "openai",
+					api_key_name = "DEEPSEEK_API_KEY",
+					endpoint = "https://api.deepseek.com",
+					model = "deepseek-chat",
+				},
 			},
 		},
 	},
