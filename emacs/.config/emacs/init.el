9ed58d98bc5b4e0f185e7e4007b69636ad944cb9
Lucas Sanchez <lucasgsanchez@gmail.com>
2026-07-16 16:05:12 -0300

    emacs - add melpa packages and kanagawa themes

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -5,20 +5,24 @@
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (menu-bar-mode -1)
+(global-display-line-numbers-mode 1)
 
 (setq visible-bell nil)
+(setq inhibit-startup-message t)
+(setq mouse-drag-copy-region t)
 
 (set-face-attribute 'default nil :family "Hack Nerd Font" :height 150)
 
-(setq inhibit-startup-message t)
-
-(global-display-line-numbers-mode 1)
-
 ;(load-theme 'modus-operandi)
 ;(load-theme 'modus-vivendi)
-(load-theme 'modus-vivendi-tinted)
+;(load-theme 'modus-vivendi-tinted)
 
-(setq mouse-drag-copy-region t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(use-package kanagawa-themes
+  :ensure t
+  :config
+  (load-theme 'kanagawa-wave t))
 
 (use-package vertico
   :ensure t
