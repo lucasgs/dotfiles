3c6e2f0c2ffefa0c61a8868e219d27479310d650
Lucas Sanchez <lucasgsanchez@gmail.com>
2026-07-20 20:11:33 -0300

    emacs - add early-init.el, load theme kanagawa

---
+++ emacs/.config/emacs/early-init.el
@@ -0,0 +1,8 @@
+(setq frame-background-mode 'dark)
+(add-to-list 'default-frame-alist '(background-color . "#192428"))
+
+(scroll-bar-mode -1)             ; Disable ugly scrollbars
+(tool-bar-mode -1)               ; Disable the graphical toolbar
+(tooltip-mode -1)                ; Disable tooltips
+(set-fringe-mode 10)             ; Give some breathing room on the sides
+(menu-bar-mode -1)               ; Disable the menu bar
